55f636ec78d60b734cbf1495a68c4a2c9609a2a8
Stanley Huang <infamous_godhand@yahoo.com>
2021-03-26 13:21:13 +1100

    showing on html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   devServer: {
     port: 8081,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./dist/index.html",
+    }),
+  ],
 };
