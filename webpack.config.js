57f88c5d553c2717d41f9ede86197f004c6a5bae
Stanley Huang <infamous_godhand@yahoo.com>
2021-03-29 10:54:16 +1100

    expose this as remote

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("./node_modules/webpack/lib/container/ModuleFederationPlugin");
 module.exports = {
   mode: "development",
   devServer: {
     port: 8081,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      fileName: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./dist/index.html",
     }),
