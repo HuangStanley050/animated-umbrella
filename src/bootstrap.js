eeaf0a4119702e3c14b4a752ac23ae5c3a6f4fe8
Stanley Huang <infamous_godhand@yahoo.com>
2021-04-07 10:31:04 +1000

    start refactoring

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,10 +1,18 @@
 import faker from "faker";
 
-let products = "";
+export const mount = (htmlElement) => {
+  let products = "";
 
-for (let i = 0; i < 3; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 3; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+  htmlElement.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.getElementById("dev-products");
+  if (el) {
+    mount();
+  }
 }
-
-document.getElementById("dev-products").innerHTML = products;
